f73a44d7a3b171267fdac8c482689293ac59d742
tytyt <1714527365@qq.com>
2019-02-13 16:06:40 +0800

    biaodan

---
+++ web/js/index.js
@@ -0,0 +1,13 @@
+function hideothers(){
+    $(".window1").show();
+    $(".window2").hide();
+    $(".window3").hide();
+};
+function nextpage1(){
+    $(".window1").hide();
+    $(".window2").show();
+};
+function nextpage2(){
+    $(".window2").hide();
+    $(".window3").show();
+};
